82d5c863dff078743cee8d0dfa6696ea829cb5f2
ernazaresanbekov <ernazar.03@icloud.com>
2023-03-22 00:22:15 +0600

    Generic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(4);
+        list.add(14);
+        list.add(54);
+        method(list);
+    }
+    public static <T> void method(List<T> t) {
+        if (t == null || t.isEmpty()) {
+            return;
+        }
+        System.out.println("First element : " + t.get(0));
+        System.out.println("Last element : " + t.get(t.size() - 1));
+        System.out.println("<~~~~~~~~~~~~~~~~~~~~~~~~~>");
+        for (T t1 : t) {
+            System.out.println("Element : " + t1);
+        }
     }
 }
